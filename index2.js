0abc62d484a42d80351f692b78545015a07b1b45
Jakki Rajasekhar <jakkirajasekhar@gmail.com>
2023-08-17 13:59:04 +0530

    final commit

--- index2.js
+++ index2.js
@@ -1,32 +1,26 @@
-// Function to open the login popup
 function openLoginPopup() {
     document.getElementById("overlay").style.display = "block";
     document.getElementById("loginForm").style.display = "block";
   }
   
-  // Function to close the login popup
   function closeLoginPopup() {
     document.getElementById("overlay").style.display = "none";
     document.getElementById("loginForm").style.display = "none";
   }
   
   
-  // Function to handle login form submission
   function login(event) {
     event.preventDefault();
   
-    // Get entered login data
     const loginUsername = document.getElementById("loginUsername").value;
     const loginPassword = document.getElementById("loginPassword").value;
   
-    // Retrieve stored signup data
     const storedUsername = localStorage.getItem("username");
     const storedPassword = localStorage.getItem("password");
   
-    // Check if entered data matches stored data
     if (loginUsername === storedUsername && loginPassword === storedPassword) {
         alert("Login successful!");
-        window.location.href = "home.html"; // Redirect to the home page
+        window.location.href = "home.html"; 
     } else {
         alert("Login failed. Check your credentials.");
     }
--- script.js
+++ script.js
@@ -1,32 +1,24 @@
-// Function to open the signup popup
 function openSignupPopup() {
     document.getElementById("overlay-signup").style.display = "block";
     document.getElementById("signupForm").style.display = "block";
 }
 
-// Function to close the signup popup
 function closeSignupPopup() {
     document.getElementById("overlay-signup").style.display = "none";
     document.getElementById("signupForm").style.display = "none";
 }
 
-// Function to handle signup form submission
 function signup(event) {
     event.preventDefault();
 
-    // Get the entered data
     const username = document.getElementById("signupUsername").value;
     const password = document.getElementById("signupPassword").value;
 
-    // Store data in local storage
     localStorage.setItem("username", username);
     localStorage.setItem("password", password);
 
-    // Close the signup popup
     closeSignupPopup();
 }
-
-// Automatically open the signup popup after a few seconds
 setTimeout(openSignupPopup, 200);
 
 
